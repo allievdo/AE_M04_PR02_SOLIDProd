6846b0c2c8d55263b510c3eff1a69615872a470c
allikitten <allison.evdokimo@cox.net>
2023-11-17 17:39:30 -0700

    :)

---
+++ SOLID Project/Assets/_Project/Scripts/Selectable.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Selectable : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI lookPercentageLabel;
+
+    [HideInInspector] public float LookPercentage;
+
+    private void Update()
+    {
+        lookPercentageLabel.text = LookPercentage.ToString(format: "F3");
+    }
+}
---
+++ SOLID Project/Assets/_Project/Scripts/SelectionManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Xml.Serialization;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.SceneManagement;
+
+public class SelectionManager : MonoBehaviour
+{
+    private IRayProvider _rayProvider;
+    private ISelector _selector;
+    private ISelectionResponse _selectionResponse;
+
+    private Transform _currentSelection;
+
+    private void Awake()
+    {
+        SceneManager.LoadScene("Environment", LoadSceneMode.Additive);
+        SceneManager.LoadScene("UI", LoadSceneMode.Additive);
+
+        _rayProvider = GetComponent<IRayProvider>();
+        _selector = GetComponent<ISelector>();
+        _selectionResponse = GetComponent<ISelectionResponse>();
+    }
+
+    private void Update()
+    {
+        //Deselection/Selection Response
+        if (_currentSelection != null) _selectionResponse.OnDeselect(_currentSelection);
+
+        //Selection Determination
+        _selector.Check(_rayProvider.CreateRay());
+        _currentSelection = _selector.GetSelection();
+
+        if (_currentSelection != null) _selectionResponse.OnSelect(_currentSelection);
+    }
+}
