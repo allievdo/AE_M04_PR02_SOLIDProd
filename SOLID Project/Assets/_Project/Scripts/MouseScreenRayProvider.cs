db8e23afdff255c9f254ea2d1227dd34c9788391
allikitten <allison.evdokimo@cox.net>
2023-12-04 13:36:34 -0700

    ;)

--- SOLID Project/Assets/_Project/Scripts/MouseScreenRayProvider.cs
+++ SOLID Project/Assets/_Project/Scripts/MouseScreenRayProvider.cs
@@ -6,6 +6,6 @@ public class MouseScreenRayProvider : MonoBehaviour, IRayProvider
 {
     public Ray CreateRay()
     {
-        return Camera.main.ScreenPointToRay(Input.mousePosition);
+        return Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
     }
 }
